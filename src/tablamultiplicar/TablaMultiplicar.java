6de48f771168912dbdd4d410be892d5d1bd4be19
Héctor Fernández Hompanera <Geralt741@gmail.com>
2021-02-14 02:05:27 +0100

    ULTRA FIX

--- src/tablamultiplicar/TablaMultiplicar.java
+++ src/tablamultiplicar/TablaMultiplicar.java
@@ -8,7 +8,6 @@ package tablamultiplicar;
 import java.util.Scanner;
 
 /**
- *
  * @author Kevin(K7)
  */
 public class TablaMultiplicar {
@@ -17,12 +16,19 @@ public class TablaMultiplicar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int n, c=0;
+        String n;
+        int c = 0;
         System.out.println("Introduce un numero");
-        Scanner num= new Scanner(System.in);
-        n=num.nextInt();
-        for(c=0;c<=10;c=c+1)
-            System.out.println(n+"*"+c+"="+n*c);
+        Scanner num = new Scanner(System.in);
+        n = num.next();
+        for (c = 0; c <= 10; c = c + 1) {
+            try {
+                System.out.println(n + "*" + c + "=" + Integer.parseInt(n) * c);
+            } catch (Exception e) {
+                System.out.println("puta aprende a leer");
+                return;
+            }
+        }
     }
-    
+
 }
